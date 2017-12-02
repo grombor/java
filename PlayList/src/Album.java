ef5dd4f610ac103977daa2c699e80554b59e4ad1
grombor <gromgaming@yahoo.com>
2017-12-02 10:10:58 +0000

    Added Playlist classfile

--- PlayList/src/Album.java
+++ PlayList/src/Album.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
 
 public class Album {
-
+	
+	private String name;
+	private ArrayList<Song> songs;
+	
+	public Album(String name) {
+		this.name = name;
+		this.songs = new ArrayList<Song>();
+	}
+	
+	
 }
--- PlayList/src/Song.java
+++ PlayList/src/Song.java
@@ -1,4 +1,14 @@
 
 public class Song {
+	
+	private String name;
+	private double duration;
+	
+	public Song(String name, double duration) {
+		this.name = name;
+		this.duration = duration;
+	}
+	
+	
 
 }
